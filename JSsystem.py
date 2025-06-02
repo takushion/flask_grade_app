258b2b70b7acbc4d39b16479969871f856ed3c91
Akari_okym <s2312477@u.tsukuba.ac.jp>
2025-06-02 12:57:58 +0900

    グラフをJavascript表記するように変更した。そのためのpythonファイルとhtmlファイルを新規作成した

---
+++ JSsystem.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+# Excel 読み込み
+df = pd.read_excel("date/grades_full.xlsx", sheet_name="average")
+
+@app.route("/report.html")
+def report():
+    return render_template("report.html")
+
+@app.route('/keyword', methods=['POST'])
+def keyword():
+    search_type = request.form['search_type']
+    keyword = request.form['word'].strip()
+
+    # 科目名 or 科目番号で検索
+    if search_type == "title":
+        matches = df[df["科⽬名称"].str.contains(keyword, na=False)]
+    else:
+        matches = df[df["科⽬番号"].astype(str).str.contains(keyword, na=False)]
+
+    results = matches.to_dict(orient='records')
+    count = len(results)
+
+    return render_template('result.html', results=results, count=count)
+
+@app.route('/graph/<subject_id>')
+def show_graph(subject_id):
+    match = df[df["科⽬番号"].astype(str) == subject_id]
+    if match.empty:
+        return "科目が見つかりませんでした"
+
+    row = match.iloc[0]
+    counts = [int(row[grade]) for grade in ["A_plus_members", "A_members", "B_members", "C_members", "D_members"]]
+    subject_name = row["科⽬名称"]
+    print(counts)
+    
+    return render_template("jsgraph.html", counts=counts, subject_name=subject_name)
+
+@app.route('/back', methods=['POST'])
+def back():
+    return render_template('report.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
