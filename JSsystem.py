f3c672047639da4d05d07673da56ebea89978eb4
Akari_okym <s2312477@u.tsukuba.ac.jp>
2025-06-02 15:23:36 +0900

    resultのテーブルに年度を追加し、各年度に飛べるようにした。そして円グラフ表示をしたところに年度切り替えを追加

--- JSsystem.py
+++ JSsystem.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 import pandas as pd
 
 app = Flask(__name__)
-
-# Excel 読み込み
-df = pd.read_excel("date/grades_full.xlsx", sheet_name="average")
+# excel 読み込み
+year = "average"  # デフォルトのシート名
+df = pd.read_excel("date/grades_full.xlsx", sheet_name=year)
 
 @app.route("/report.html")
 def report():
@@ -14,7 +14,7 @@ def report():
 def keyword():
     search_type = request.form['search_type']
     keyword = request.form['word'].strip()
-
+    
     # 科目名 or 科目番号で検索
     if search_type == "title":
         matches = df[df["科⽬名称"].str.contains(keyword, na=False)]
@@ -28,6 +28,8 @@ def keyword():
 
 @app.route('/graph/<subject_id>')
 def show_graph(subject_id):
+    year = request.args.get('year', 'average') 
+    df = pd.read_excel("date/grades_full.xlsx", sheet_name=year)
     match = df[df["科⽬番号"].astype(str) == subject_id]
     if match.empty:
         return "科目が見つかりませんでした"
@@ -35,9 +37,9 @@ def show_graph(subject_id):
     row = match.iloc[0]
     counts = [int(row[grade]) for grade in ["A_plus_members", "A_members", "B_members", "C_members", "D_members"]]
     subject_name = row["科⽬名称"]
-    print(counts)
-    
-    return render_template("jsgraph.html", counts=counts, subject_name=subject_name)
+    subject_id = row["科⽬番号"]
+
+    return render_template("jsgraph.html", counts=counts, subject_name=subject_name, subject_id=subject_id, year=year)
 
 @app.route('/back', methods=['POST'])
 def back():
